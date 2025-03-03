02fd6433c2ee6f53bc49c67816286ba078ff6854
Tohid <mrtohid0020@gmail.com>
2025-03-04 01:13:20 +0600

    process app for exam 5

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ostad_assingment/app.dart';//
+import 'package:ostad_assingment/app.dart';
 
 import 'package:ostad_assingment/main.dart';
 
